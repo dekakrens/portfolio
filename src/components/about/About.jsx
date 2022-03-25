c29e33fc65276c08b75390125bccb3c61232e7bc
Deny Kurniawan <dekakrens@gmail.com>
2022-03-25 23:42:30 +0700

    About is done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,56 @@
 import React from "react";
+import { FaAward } from "react-icons/fa";
+import { FiUser } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+import ME from "../../assets/me-about.jpg";
 import "./about.css";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUser className="about__icon" />
+              <h5>Clients</h5>
+              <small>2 Local</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>5+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            sunt porro, ipsa, dolorem dolore dolorum voluptas incidunt vel hic,
+            praesentium dolores placeat aliquid animi numquam. Qui facere
+            aliquam recusandae blanditiis.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
